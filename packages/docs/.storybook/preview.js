b67b6c228db49cfae870cd85e5e889086eece3db
ruanosena <ruosena@hotmail.com>
2024-02-08 09:49:40 -0300

    design system em React

---
+++ packages/docs/.storybook/preview.js
@@ -0,0 +1,21 @@
+import { themes } from '@storybook/theming';
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    docs: {
+      theme: themes.dark
+    },
+    backgrounds: {
+      default: "dark"
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
